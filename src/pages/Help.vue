5ae6c1a56e95e941ef389c39693981f79820ff25
Sealman <castle2668@gmail.com>
2020-04-05 23:55:25 +0800

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam ea atque quaerat et officiis quis iste corrupti consequatur libero consectetur, commodi repellat alias incidunt nobis facilis doloribus minus ullam.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, nihil. Delectus dicta laboriosam voluptates et quos aperiam? Itaque tenetur minima aspernatur inventore laudantium accusamus, eum minus doloribus tempore cupiditate iusto.</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
